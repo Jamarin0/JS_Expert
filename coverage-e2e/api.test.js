6e099e0080dcccb7d09a99d2c23b62e593337060
Jamarin0 <jgjsguilherme@gmail.com>
2022-01-17 00:32:28 -0300

    add test coverage 100%

--- coverage-e2e/api.test.js
+++ coverage-e2e/api.test.js
@@ -10,10 +10,23 @@ describe("API Suite test", () => {
       assert.deepStrictEqual(response.text, "contact us page");
     });
   });
+
   describe("/helo", () => {
     it("should request an inexistent route /hi and redirect to /helo", async () => {
-        const response = await request(app).get("/hi").expect(200);
-        assert.deepStrictEqual(response.text, "Hello world!");
+      const response = await request(app).get("/hi").expect(200);
+      assert.deepStrictEqual(response.text, "Hello world!");
     });
+  });
+
+  describe("/login", () => {
+    it("should login successfully on the login route and return HTTP Status 200", async () => {
+      const response = await request(app).post("/login").send({username: "Guilherme", password: "123"}).expect(200);
+      assert.deepStrictEqual(response.text, "Logging has succeeded!");
+    });
+    it("should unauthorize a request when requesting it using wrong credentials and return HTTP Status 401", async () => {
+        const response = await request(app).post("/login").send({username: "Irish", password: "123"}).expect(401);
+        assert.ok(response.unauthorized)
+        assert.deepStrictEqual(response.text, "Logging failed");
+      });
   });
 });
