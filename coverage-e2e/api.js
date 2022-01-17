a0a37989ff409bcb2fc075f87364fc90251598a6
Jamarin0 <jgjsguilherme@gmail.com>
2022-01-17 00:11:56 -0300

    add coverage

--- coverage-e2e/api.js
+++ coverage-e2e/api.js
@@ -1,11 +1,30 @@
 const http = require("http");
 
+const routes = {
+  "/contact:get": (request, response) => {
+    response.write("contact us page");
+    return response.end();
+  },
+
+  default: (request, response) => {
+    response.write("Hello world!");
+    return response.end();
+  },
+};
+
 const handler = function (request, response) {
-  return response.end("Helo World!");
+  const { url, method } = request;
+  const routekey = `${url}:${method.toLowerCase()}`;
+
+  const chosen = routes[routekey] || routes.default;
+  response.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  return chosen(request, response);
 };
 
 const app = http
   .createServer(handler)
   .listen(3000, () => console.log("app running at", 3000));
 
-  module.exports = app
+module.exports = app;
---
+++ coverage-e2e/api.test.js
@@ -0,0 +1,19 @@
+const { describe, it } = require("mocha");
+const request = require("supertest");
+const app = require("./api");
+const assert = require("assert");
+
+describe("API Suite test", () => {
+  describe("/contact", () => {
+    it("should request the contact page and return HTTP Status 200", async () => {
+      const response = await request(app).get("/contact").expect(200);
+      assert.deepStrictEqual(response.text, "contact us page");
+    });
+  });
+  describe("/helo", () => {
+    it("should request an inexistent route /hi and redirect to /helo", async () => {
+        const response = await request(app).get("/hi").expect(200);
+        assert.deepStrictEqual(response.text, "Hello world!");
+    });
+  });
+});
