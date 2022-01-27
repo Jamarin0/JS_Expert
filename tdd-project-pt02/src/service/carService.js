c91acc45c2c6c53907c7508be020aa22448cad57
Jamarin0 <jgjsguilherme@gmail.com>
2022-01-26 21:53:21 -0300

    add CarService TDD

--- tdd-project-pt02/src/service/carService.js
+++ tdd-project-pt02/src/service/carService.js
@@ -1,13 +1,27 @@
-const BaseRepository = require('./../repository/baseRepository')
+const BaseRepository = require("./../repository/baseRepository");
 
 class CarService {
-    constructor({cars}){
-        this.carRepository = new BaseRepository({ file: cars})
-    }
+  constructor({ cars }) {
+    this.carRepository = new BaseRepository({ file: cars });
+  }
 
-    async getAvailableCar(carCategory) {
-        return null
-    }
+  getRadomPositionFromArray(list) {
+    const listLength = list.length;
+    return Math.floor(Math.random() * listLength);
+  }
+
+  chooseRandomCar(carCategory) {
+    const randomCarIndex = this.getRadomPositionFromArray(carCategory.carIds);
+    const carId = carCategory.carIds[randomCarIndex];
+
+    return carId;
+  }
+
+  async getAvailableCar(carCategory) {
+    const carId = this.chooseRandomCar(carCategory);
+    const car = await this.carRepository.find(carId);
+    return car;
+  }
 }
 
-module.exports = CarService
+module.exports = CarService;
--- tdd-project-pt02/test/unitTests/carService.test.js
+++ tdd-project-pt02/test/unitTests/carService.test.js
@@ -1,32 +1,66 @@
-const { describe, it, before } = require("mocha");
+const { describe, it, before, beforeEach, afterEach } = require("mocha");
 const CarService = require("../../src/service/carService");
 
 const { join } = require("path");
-const assert = require('assert')
+const { expect } = require("chai");
+const sinon = require("sinon");
 
 const carsDatabase = join(__dirname, "./../../database", "cars.json");
 
 const mocks = {
-    validCarCategory: require('./../mocks/valid-carCategory.json'),
-    validCar: require('./../mocks/valid-car.json'),
-    validCustomer: require('./../mocks/valid-customer.json')
-}
+  validCarCategory: require("./../mocks/valid-carCategory.json"),
+  validCar: require("./../mocks/valid-car.json"),
+  validCustomer: require("./../mocks/valid-customer.json"),
+};
 
 describe("CarService Suite Tests", () => {
   let carService = {};
+  let sandbox = {};
+
   before(() => {
     carService = new CarService({
       cars: carsDatabase,
     });
   });
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("should retrive a random position from an array", () => {
+    const data = [0, 1, 2, 3, 4];
+    const result = carService.getRadomPositionFromArray(data);
+
+    expect(result).to.be.lte(data.length).and.be.gte(0);
+  });
+
+  it("should choose the first id from carIds in carCategory", () => {
+    const carCategory = mocks.validCarCategory;
+    const carIndex = 0;
+
+    sandbox
+      .stub(carService, carService.getRadomPositionFromArray.name)
+      .returns(carIndex);
+
+    const result = carService.chooseRandomCar(carCategory);
+    const expected = carCategory.carIds[carIndex];
+
+    expect(carService.getRadomPositionFromArray.calledOnce).to.be.ok;
+    expect(result).to.be.equal(expected);
+  });
+
   it("given a carCategory it should return an available car", async () => {
-      const car = mocks.validCar
-      const carCategory = Object.create(mocks.carCategory)
-      carCategory.ids = [car.id]
-      
-      const result = await carService.getAvailableCar()
-      const expected = {}
-      assert.deepStrictEqual(result, expected)
-      console.log('result', result)
+    const car = mocks.validCar;
+    const carCategory = Object.create(mocks.validCarCategory);
+    carCategory.carIds = [car.id];
+
+    const result = await carService.getAvailableCar(carCategory);
+    const expected = car;
+
+    expect(result).to.be.deep.equal(expected);
   });
 });
