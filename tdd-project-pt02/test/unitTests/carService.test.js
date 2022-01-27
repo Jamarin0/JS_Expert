9dc2c2b221a605d29fa0bdbeba359d9917749da6
Jamarin0 <jgjsguilherme@gmail.com>
2022-01-26 22:07:33 -0300

    add stub and spy finally tests

--- tdd-project-pt02/test/unitTests/carService.test.js
+++ tdd-project-pt02/test/unitTests/carService.test.js
@@ -58,9 +58,22 @@ describe("CarService Suite Tests", () => {
     const carCategory = Object.create(mocks.validCarCategory);
     carCategory.carIds = [car.id];
 
+    sandbox.stub(
+        carService.carRepository,
+        carService.carRepository.find.name,
+    ).resolves(car)
+
+    sandbox.spy(
+        carService,
+        carService.chooseRandomCar.name,
+    )
+
     const result = await carService.getAvailableCar(carCategory);
     const expected = car;
 
+    expect(carService.chooseRandomCar.calledOnce).to.be.ok;
+    expect(carService.carRepository.find.calledWithExactly(car.id)).to.be.ok;
     expect(result).to.be.deep.equal(expected);
   });
 });
+ 
