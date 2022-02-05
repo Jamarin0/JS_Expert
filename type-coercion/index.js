600bf49c809d54a966fa2e57864bcb4b48c47c21
Jamarin0 <jgjsguilherme@gmail.com>
2022-02-05 01:45:29 -0300

    coercion

--- type-coercion/index.js
+++ type-coercion/index.js
@@ -40,12 +40,47 @@ console.assert(('Hello' && 123) === 123, "&& return de last element!")
 const item = {
     name: 'Guilherme Jamarino',
     age: 21,
+    // string: 1 se nao for primitivo, chama o valueOf
     toString(){
         return `Name: ${this.name}, Age: ${this.age}` 
     },
+    // number: 1 se nao for primitvo,chama o toString
     valueOf(){
-        return 007
+        return {hey: 'dude'}
+        // return 007
+    },
+    //ele tem prioridade
+    [Symbol.toPrimitive](coercionType) {
+        // console.log('trying to convert to', coercionType)
+        const types = {
+            string: JSON.stringify(this),
+            number: '0007'
+        }
+
+        return types[coercionType] || types.string
     }
 }
 
-console.log('item', item + 0)
+// console.log('toString', String(item))
+//// vai retornar o NaN pois o toString retornou a string
+// console.log('valueOf', Number(item))
+
+// depois de adicionar o primitive
+// console.log('String', String(item))
+// console.log('Number', Number(item))
+// //chama a conversão default
+// console.log('Date', new Date(item))
+
+console.assert(item + 0 === '{"name":"Guilherme Jamarino","age":21}0')
+// console.log('!!item is true', !!item)
+console.assert(!!item)
+
+// console.log('string.concat', 'Ae'.concat(item))
+console.assert('Ae'.concat(item) === 'Ae{"name":"Guilherme Jamarino","age":21}')
+
+// console.log('implicit + explicit coercion (using ==)',item == String(item))
+console.assert(item == String(item))
+
+const item2 = { ...item, name: "jj", age: 99}
+// console.log('New Obj', item2) 
+console.assert(item2.name === "jj" && item2.age === 99)
